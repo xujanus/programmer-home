f9c8fd5892a2c9b081e2a8e1306925b3a6e821c7
xujun <xujun@zjrealtech.com>
2018-05-08 20:16:38 +0800

    feat:scaffold项目增加项目配置相关代码 subject:提供了读取配置、切换配置的相关代码和注释

--- scaffold/src/main/java/com/programer/scaffold/controller/restful/HelloController.java
+++ scaffold/src/main/java/com/programer/scaffold/controller/restful/HelloController.java
@@ -1,5 +1,6 @@
 package com.programer.scaffold.controller.restful;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -11,10 +12,18 @@ import org.springframework.web.bind.annotation.RestController;
  * <p>Date           2018/5/8
  */
 @RestController
+@RequestMapping("/api")
 public class HelloController {
+    @Value("${test.text}")
+    private String textInSysConfig;
 
     @RequestMapping("/hello")
     public String helloWorld() {
         return "Hello world!";
+    }
+
+    @RequestMapping("/sys-config/test")
+    public String sysConfigTest() {
+        return textInSysConfig;
     }
 }
---
+++ scaffold/src/test/java/com/programer/scaffold/controller/restful/HelloControllerTest.java
@@ -0,0 +1,41 @@
+package com.programer.scaffold.controller.restful;
+
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.springframework.test.context.web.WebAppConfiguration;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+/**
+ * <p>ClassName:     HelloControllerTest
+ * <p>Description:   单元测试
+ * <p>Author         xu jun
+ * <p>Version        V1.0
+ * <p>Date           2018/5/8
+ */
+@RunWith(SpringJUnit4ClassRunner.class)
+@SpringBootTest
+@WebAppConfiguration
+public class HelloControllerTest {
+    private MockMvc mvc;
+
+    @Before
+    public void setUp() {
+        mvc = MockMvcBuilders.standaloneSetup(new HelloController()).build();
+    }
+
+    @Test
+    public void testHelloWorld() throws Exception {
+        mvc.perform(MockMvcRequestBuilders.get("/hello").accept(MediaType.APPLICATION_JSON))
+                .andExpect(status().isOk())
+                .andExpect(content().string("Hello world!"));
+    }
+}
